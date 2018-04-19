92434c6fa25c2a671d8493b720dd7fcbff3355b4
Oleg Polyakov <olegpolyako@outlook.com>
2018-04-19 15:12:54 +0300

    Add Stack tests

---
+++ stack/test.js
@@ -0,0 +1,90 @@
+const Stack = require('./');
+
+xdescribe('Stack', () => {
+    describe('Stack.constructor()', () => {
+        test('возвращает объект типа `Stack`', () => {
+            expect(new Stack()).toBeInstanceOf(Stack);
+        });
+
+        test('принимает элементы для добавления в стопку', () => {
+            let stack = new Stack(1, 2, 3);
+
+            expect(stack.size).toBe(3);
+        });
+    });
+
+    describe('Stack.size', () => {
+        test('возвращает количество элементов в стопке', () => {
+            let stack = new Stack(1, 2, 3);
+
+            expect(stack.size).toBe(3);
+        });
+    });
+
+    describe('Stack.isEmpty', () => {
+        test('возвращает `true` если стопка пустая', () => {
+            let stack = new Stack();
+
+            expect(stack.isEmpty).toBe(true);
+        });
+
+        test('возвращает `false` если стопка не пустая', () => {
+            let stack = new Stack(1, 2, 3);
+
+            expect(stack.isEmpty).toBe(false);
+        });
+    });
+
+    describe('Stack.push()', () => {
+        test('добавляет элемент в конец стопки', () => {
+            let stack = new Stack();
+
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+    
+            expect(stack.size).toBe(3);
+        });
+    });
+
+    describe('Stack.pop()', () => {
+        test('удаляет последний элемент из стопки', () => {
+            let stack = new Stack();
+
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
+            stack.pop();
+    
+            expect(stack.size).toBe(2);
+        });
+
+        test('возвращает последний элемент в стопке', () => {
+            let stack = new Stack();
+
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
+            let item = stack.pop();
+    
+            expect(item).toBe(3);
+        });
+    });
+
+    describe('Stack.push()', () => {
+        test('возвращает последний элемент в стопке не удаляя его', () => {
+            let stack = new Stack();
+
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
+            let item = stack.peek();
+    
+            expect(stack.size).toBe(3);
+            expect(item).toBe(3);
+        });
+    });
+});
