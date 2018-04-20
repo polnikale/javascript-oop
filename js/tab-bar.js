141509bebaea3c2550d094481b4b5e70f3f11699
Oleg Polyakov <olegpolyako@outlook.com>
2018-04-20 22:34:23 +0300

    Add tab bar class definition

--- js/tab-bar.js
+++ js/tab-bar.js
@@ -1,3 +1,53 @@
 export default class TabBar {
-    
+    /**
+     * Создает объект.
+     * @param {{ element: HTMLElement, tabs: Tab[], onChange: Function }} args
+     */
+    constructor() {
+
+    }
+
+    /**
+     * Инициализирует объект.
+     * Устанавливает обработчик для обработки активации вкладки.
+     * @private
+     */
+    init() {
+        
+    }
+
+    /**
+     * Возвращает HTML элемент.
+     * @returns {HTMLElement}
+     */
+    get element() {  }
+
+    /**
+     * Возвращает массив вкладок.
+     * @returns {Tab[]}
+     */
+    get tabs() {  }
+
+    /**
+     * Возвращает активную вкладку.
+     * @returns {Tab}
+     */
+    get activeTab() {  }
+
+    /**
+     * Возвращает индекс активной вкладки.
+     * @returns {number}
+     */
+    get activeTabIndex() {  }
+
+    /**
+     * Вызывается при нажатии по вкладке.
+     * Делает все вкладки кроме активной неактивными.
+     * Вызывает функцию обратно вызова, отправляя туда активную вкладку.
+     * @private
+     * @param {Tab} activeTab 
+     */
+    handleActivate() {
+        
+    }
 }
