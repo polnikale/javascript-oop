a136786ff03cf2548c9275d75376e5a22d62fb1a
Oleg Polyakov <olegpolyako@outlook.com>
2018-05-02 12:38:00 +0300

    Start

---
+++ js/question.js
@@ -0,0 +1,20 @@
+export default class Question {
+    /**
+     * @param {string} text Текст вопроса
+     * @param {string[]} answers Варианты ответов
+     * @param {number} correctAnswer Индекс правильного ответа
+     */
+    constructor(text, answers, correctAnswer) {
+
+    }
+
+    /**
+     * Проверяет правильность ответа.
+     * 
+     * @param {number} answer
+     * @returns {boolean}
+     */
+    isCorrectAnswer(answer) {
+        
+    }
+}
---
+++ js/quiz.js
@@ -0,0 +1,35 @@
+export default class Quiz {
+    /**
+     * @param {string} title 
+     * @param {Question[]} quiestions 
+     */
+    constructor(title, quiestions) {
+
+    }
+
+    /**
+     * Возвращает текущий вопрос.
+     * 
+     * @returns {Question}
+     */
+    get currentQuestion() {
+        
+    }
+
+    /**
+     * Возвращает `true/false` в зависимости от того закончился тест или нет.
+     * 
+     * @returns {boolean}
+     */
+    get hasEnded() {
+        
+    }
+
+    /**
+     * Проверяет правильность ответа выбранного пользователем.
+     * @param {*} answer 
+     */
+    checkAnswer(answer) {
+        
+    }
+}
