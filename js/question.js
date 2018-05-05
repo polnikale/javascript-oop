37df2c458ad40c1fef4a9c3eee444f1bb8cffc4b
polnikale <nikpolale@gmail.com>
2018-05-06 00:50:51 +0300

    initialized ts. Finished question.

--- js/question.js
+++ js/question.js
@@ -1,20 +1,20 @@
-export default class Question {
-    /**
-     * @param {string} text Текст вопроса
-     * @param {string[]} answers Варианты ответов
-     * @param {number} correctAnswer Индекс правильного ответа
-     */
-    constructor(text, answers, correctAnswer) {
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Question = /** @class */ (function () {
+    function Question(text, answers, correctAnswer) {
+        this.text = text;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
     }
-
     /**
      * Проверяет правильность ответа.
-     * 
+     *
      * @param {number} answer
      * @returns {boolean}
      */
-    isCorrectAnswer(answer) {
-        
-    }
-}
+    Question.prototype.isCorrectAnswer = function (answer) {
+        return answer === this.correctAnswer;
+    };
+    return Question;
+}());
+exports.default = Question;
---
+++ ts/question.ts
@@ -0,0 +1,25 @@
+export default class Question {
+  /**
+   * @param {string} text Текст вопроса
+   * @param {string[]} answers Варианты ответов
+   * @param {number} correctAnswer Индекс правильного ответа
+   */
+  text: string;
+  answers: string[];
+  correctAnswer: number;
+  constructor(text: string, answers: string[], correctAnswer: number) {
+    this.text = text;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+
+  /**
+   * Проверяет правильность ответа.
+   * 
+   * @param {number} answer
+   * @returns {boolean}
+   */
+  isCorrectAnswer(answer: number): boolean {
+    return answer === this.correctAnswer;
+  }
+}
