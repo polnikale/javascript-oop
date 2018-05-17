407cbf1f6e32c58de451914c15b6da873aad5cee
polnikale <nikpolale@gmail.com>
2018-05-17 18:55:37 +0300

    handling click on palette

--- js/color-palette.js
+++ js/color-palette.js
@@ -2,6 +2,7 @@ export default class ColorPalette {
     constructor({ element, colors }) {
         this.element = element;
         this.colors = colors;
+        this.element.addEventListener('click', this.colorPaletteHandler.bind(this));
         this.init();
     }
     init() {
@@ -11,5 +12,19 @@ export default class ColorPalette {
             colorLi.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
             this.element.appendChild(colorLi);
         });
+    }
+    colorPaletteHandler(event) {
+        let target = event.target;
+        if (!target) {
+            return;
+        }
+        if (target.tagName !== 'LI') { // clicked out of the circle
+            return;
+        }
+        if (this.activeIndex != null) {
+            this.element.childNodes[this.activeIndex].classList.remove('active');
+        }
+        this.activeIndex = Array.from(this.element.childNodes).indexOf(target);
+        target.classList.add('active');
     }
 }
--- ts/color-palette.ts
+++ ts/color-palette.ts
@@ -3,15 +3,19 @@ type IColor =  { red: number, green: number, blue: number }[];
 interface IColorPalette {
   readonly element: Element;
   colors: IColor;
+  activeIndex: number;
 }
 
 
 export default class ColorPalette implements IColorPalette {
   readonly element: Element;
   colors: IColor;
+  activeIndex!: number;
   constructor({element, colors}: {element: Element, colors: IColor}) {
     this.element = element;
     this.colors = colors;
+
+    this.element.addEventListener('click', this.colorPaletteHandler.bind(this));
 
     this.init();
   }
@@ -23,5 +27,21 @@ export default class ColorPalette implements IColorPalette {
       colorLi.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
       this.element.appendChild(colorLi);
     });
+  }
+
+  colorPaletteHandler(event: MouseEvent): void {
+    let target = event.target;
+    if (!target) {
+      return;
+    }
+    if (target.tagName !== 'LI') { // clicked out of the circle
+      return;
+    }
+
+    if (this.activeIndex != null) {
+      this.element.childNodes[this.activeIndex].classList.remove('active');
+    }
+    this.activeIndex = Array.from(this.element.childNodes).indexOf(target);
+    target.classList.add('active');
   }
 }
