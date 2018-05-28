05adfaa4153395ee2cfcb75ef890ec04f2346ae4
polnikale <nikpolale@gmail.com>
2018-05-28 23:41:51 +0300

    Added catching emitted events in game.js

--- js/game.js
+++ js/game.js
@@ -5,12 +5,25 @@ export default class Game {
   constructor({size, gameElem}) {
     this.grid = new Grid({size, gridElem: gameElem.querySelector('#grid')});
     this.controls = new Controls(gameElem.querySelector('.controls')); // controlsElem
+
+    this.handleClear = this.handleClear.bind(this);
+    this.handleNewLayer = this.handleNewLayer.bind(this);
+    this.handleRandomize = this.handleRandomize.bind(this);
   
-    this.controls.on('createNewLayer', () => {
-      console.log('ok');
-    });
-    this.controls.on('clear', () => {
-      console.log('cleeeaaaariiing');
-    });
+    this.controls.on('createNewLayer', this.handleNewLayer);
+    this.controls.on('clear', this.handleClear);
+    this.controls.on('randomize', this.handleRandomize);
+  }
+
+  handleClear() {
+
+  }
+
+  handleNewLayer() {
+
+  }
+
+  handleRandomize() {
+
   }
 }
