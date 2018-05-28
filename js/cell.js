e012a15766b7fc1f8ecbe2adbdbb5ab6bf7e9299
polnikale <nikpolale@gmail.com>
2018-05-28 20:38:59 +0300

    handle cell clicks

--- js/cell.js
+++ js/cell.js
@@ -2,5 +2,26 @@ export default class Cell {
   constructor({trElem}) {
     this.cell = document.createElement('td');
     trElem.appendChild(this.cell);
+
+    this.handleChangeAliveStatus = this.handleChangeAliveStatus.bind(this);
+
+    this.init();
+  }
+
+  init() {
+    this._alive = false;
+    this.cell.addEventListener('click', this.handleChangeAliveStatus);
+  }
+
+  get alive() {
+    return this._alive;
+  }
+  set alive(value) {
+    this._alive = value;
+    this.cell.classList.toggle('alive', this._alive);
+  }
+
+  handleChangeAliveStatus() {
+    this.alive = !this.alive;
   }
 }
