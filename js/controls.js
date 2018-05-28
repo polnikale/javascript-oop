692df6ac22e5a048df6c493b26bc7a380e8d0ca8
polnikale <nikpolale@gmail.com>
2018-05-28 22:02:15 +0300

    Started togglegame/resetgame events in Controls class

--- js/controls.js
+++ js/controls.js
@@ -6,10 +6,14 @@ export default class Controls extends EventEmitter {
 
     this.controlsElem = controlsElem;
 
-    this.handleStartGame = this.handleStartGame.bind(this);
+    this.handleToggleGame = this.handleToggleGame.bind(this);
     this.handleResetGame = this.handleResetGame.bind(this);
     this.handleRandomizeGame = this.handleRandomizeGame.bind(this);
     this.handleChangeSpeed = this.handleChangeSpeed.bind(this);
+    this.emitNewLayer = this.emitNewLayer.bind(this);
+
+    this.interval = null;
+    this.time = 1000;
 
     this.init()
   }
@@ -20,18 +24,25 @@ export default class Controls extends EventEmitter {
     this.randomizeBtn = this.controlsElem.querySelector('#randomize-button');
     this.speedSlider = this.controlsElem.querySelector('#speed-slider');
 
-    this.playBtn.addEventListener('click', this.handleStartGame);
+    this.playBtn.addEventListener('click', this.handleToggleGame);
     this.resetBtn.addEventListener('click', this.handleResetGame);
     this.randomizeBtn.addEventListener('click', this.handleRandomizeGame);
     this.speedSlider.addEventListener('click', this.handleChangeSpeed);
   }
 
-  handleStartGame() {
-
+  handleToggleGame() {
+    if (this.interval == null) {
+      this.interval = setInterval(() => {
+        this.emitNewLayer();
+      }, this.time); 
+    } else {
+      this.clearLayerInterval();
+    }
   }
 
   handleResetGame() {
-
+    this.clearLayerInterval();
+    this.emit('clear');
   }
   
   handleRandomizeGame() {
@@ -40,5 +51,14 @@ export default class Controls extends EventEmitter {
 
   handleChangeSpeed() {
 
+  }
+
+  emitNewLayer() {
+    this.emit('createNewLayer');
+  }
+
+  clearLayerInterval() {
+    clearInterval(this.interval);
+    this.interval = null;
   }
 }
--- js/game.js
+++ js/game.js
@@ -4,6 +4,13 @@ import Controls from './controls.js';
 export default class Game {
   constructor({size, gameElem}) {
     this.grid = new Grid({size, gridElem: gameElem.querySelector('#grid')});
-    this.controls = new Controls(gameElem.querySelector('#controls')); // controlsElem
+    this.controls = new Controls(gameElem.querySelector('.controls')); // controlsElem
+  
+    this.controls.on('createNewLayer', () => {
+      console.log('ok');
+    });
+    this.controls.on('clear', () => {
+      console.log('cleeeaaaariiing');
+    });
   }
 }
